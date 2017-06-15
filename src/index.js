0ac68e0af46d1a8e30c562ebe66582b81471d68b
Palash <palash@itobuz.com>
2017-06-15 09:52:13 +0530

    failed deploy

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 const bearsRoutes = require('./app/routes/bears');
 var port = process.env.PORT || 8080;
 
+app.use('/api', bearsRoutes(app));
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -18,5 +19,5 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-app.use('/api', bearsRoutes(app));
+
 app.listen(port);
